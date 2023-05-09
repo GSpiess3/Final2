c184bf32920dccd829ac662e39c625888cf89907
GSpiess3 <gabespiess@gmail.com>
2023-05-09 14:14:21 -0500

    9

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import os
 import sys
 import math
 import random
+from typing import Union
+
 import pygame
+from pygame import Surface, SurfaceType
 
 WIDTH = 623
 HEIGHT = 150
 
 pygame.init()
+pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('DINO GAME')
+
 
 class BackGround:
     '''
@@ -18,6 +23,7 @@ class BackGround:
     Since the dino isn't actually moving we actually move the background on a loop
 
     '''
+
     def __init__(self, x, y):
         '''
         :param width: We set the width = to our global width variable
@@ -50,7 +56,6 @@ class BackGround:
         '''
         screen.blit(self.texture, (self.X, self.Y))
 
-
     def set_texture(self):
         '''
         This one is used to make the background
@@ -61,6 +66,7 @@ class BackGround:
         path = os.path.join('assets/images/bg.png')
         self.texture = pygame.image.load(path)
         self.texture = pygame.transform.scale(self.texture, (self.width, self.height))
+
 
 class Dino:
 
@@ -78,28 +84,33 @@ class Dino:
         self.falling = False
         self.fall_stop = self.y
         self.set_texture()
+        self.set_sound()
         self.show()
 
     def update(self, loops):
         '''
         resposnible for udating the dino
         '''
-        #jumping
+        # jumping
         if self.jumping:
             self.y -= self.dy
             if self.y <= self.jump_stop:
                 self.fall()
 
-        #falling
+        # falling
         elif self.falling:
             self.y += self.gravity * self.dy
             if self.y >= self.fall_stop:
                 self.stop()
 
-        #walking
+        # walking
         if self.onground and loops % 4 == 0:
-            self.texture_num = (self.texture_num +1) % 3
+            self.texture_num = (self.texture_num + 1) % 3
             self.set_texture()
+
+    def set_sound(self):
+        path = os.path.join('assets/sounds/jump.wav')
+        self.sound = pygame.mixer.Sound(path)
 
     def show(self):
         '''
@@ -121,6 +132,7 @@ class Dino:
         used to setting the booleans to the value we need while jumping
         :return:
         '''
+        self.sound.play()
         self.jumping = True
         self.onground = True
 
@@ -139,6 +151,7 @@ class Dino:
         '''
         self.falling = False
         self.onground = True
+
 
 class Cactus:
 
@@ -161,26 +174,87 @@ class Cactus:
         self.texture = pygame.image.load(path)
         self.texture = pygame.transform.scale(self.texture, (self.width, self.height))
 
+
 class Collision:
 
     def between(self, obj1, obj2):
-        distance = math.sqrt((obj1.x - obj2.x)**2 + (obj1.y - obj2.y)**2)
+        distance = math.sqrt((obj1.x - obj2.x) ** 2 + (obj1.y - obj2.y) ** 2)
         return distance < 35
+
+
+class Score:
+
+    def __init__(self, hs):
+        self.hs = hs
+        self.act = 0
+        self.font = pygame.font.SysFont('monospace', 18)
+        self.color = (0, 0, 0)
+        self.set_sound()
+        self.show()
+
+    def update(self, loops):
+        self.act = loops // 10
+        self.check_hs()
+        self.check_sound()
+
+    def show(self):
+        self.lbl = self.font.render(f'HI {self.hs} {self.act}', 1, self.color)
+        lbl_width = self.lbl.get_rect().width
+        screen.blit(self.lbl, (WIDTH - lbl_width - 10, 10))
+
+    def set_sound(self):
+        path = os.path.join('assets/sounds/point.wav')
+        self.sound = pygame.mixer.Sound(path)
+
+
+    def check_hs(self):
+        if self.act >= self.hs:
+            self.hs = self.act
+
+    def check_sound(self):
+        if self.act % 100 == 0 and self.act != 0:
+            self.sound.play()
+
 
 class Game:
 
-    def __init__(self):
+    def __init__(self, hs=0):
         self.bg = [BackGround(0, 0), BackGround(WIDTH, 0)]
         self.dino = Dino()
         self.obstactles = []
         self.collision = Collision()
+        self.score = Score(hs)
         self.speed = 3
+        self.start_label()
+        screen.blit(self.start_big_lbl, (WIDTH / 2 - self.start_big_lbl.get_width() // 2, HEIGHT // 4))
+        screen.blit(self.start_small_lbl, (WIDTH // 2 - self.start_small_lbl.get_width() // 2, HEIGHT // 2))
         self.playing = False
+        self.set_sound()
+        self.set_labels()
 
+
+    def start_label(self):
+        big_font = pygame.font.SysFont('monospace', 24, bold=True)
+        small_font = pygame.font.SysFont('monospace', 18)
+        self.start_big_lbl = big_font.render(f'D I N O  G A M E', 1, (0, 0, 0))
+        self.start_small_lbl = small_font.render(f'Press space to start/play', 1, (0, 0, 0))
+    def set_labels(self):
+        big_font = pygame.font.SysFont('monospace', 24, bold=True)
+        small_font = pygame.font.SysFont('monospace', 18)
+        self.reset_big_lbl = big_font.render(f'G A M E  O V E R', 1, (0, 0, 0))
+        self.reset_small_lbl = small_font.render(f'Press r to restart', 1, (0, 0, 0))
+
+
+    def set_sound(self):
+        path = os.path.join('assets/sounds/die.wav')
+        self.sound = pygame.mixer.Sound(path)
     def start(self):
         self.playing = True
 
     def over(self):
+        self.sound.play()
+        screen.blit(self.reset_big_lbl, (WIDTH/2 - self.reset_big_lbl.get_width() // 2, HEIGHT // 4))
+        screen.blit(self.reset_small_lbl, (WIDTH // 2 - self.reset_small_lbl.get_width() // 2, HEIGHT// 2))
         self.playing = False
 
     def tospawn(self, loops):
@@ -194,41 +268,45 @@ class Game:
             x = random.randint(prev_cactus.x + self.dino.width + 84, WIDTH + prev_cactus.x + self.dino.width + 84)
 
 
-        #empty list
+        # empty list
         else:
             x = random.randint(WIDTH + 100, 1000)
 
-        #create new cactus
+        # create new cactus
         cactus = Cactus(x)
         self.obstactles.append(cactus)
 
-def main():
+    def restart(self):
+        self.__init__(hs = self.score.hs)
 
-    #objects
+
+def main():
+    # objects
     game = Game()
     dino = game.dino
 
+    #variables
     loops = 0
-
     clock = pygame.time.Clock()
+    over = False
+
 
     while True:
 
         if game.playing:
 
-
             loops += 1
 
-            #----BG----
+            # ----BG----
             for bg in game.bg:
                 bg.update(-game.speed)
                 bg.show()
 
-            #---Dino---
+            # ---Dino---
             dino.update(loops)
             dino.show()
 
-            #---cactus---
+            # ---cactus---
 
             if game.tospawn(loops):
                 game.spawn_cactus()
@@ -237,11 +315,18 @@ def main():
                 cactus.update(-game.speed)
                 cactus.show()
 
-                #---Collision---
+                # ---Collision---
                 if game.collision.between(dino, cactus):
-                    game.over()
+                    over = True
 
-        #events
+            if over:
+                game.over()
+
+            #---Score---
+            game.score.update(loops)
+            game.score.show()
+
+        # events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -250,13 +335,20 @@ def main():
             if event.type == pygame.KEYDOWN:
 
                 if event.key == pygame.K_SPACE:
-                    if dino.onground:
-                        dino.jump()
-                    if not game.playing:
-                        game.start()
+                    if not over:
+                        if dino.onground:
+                            dino.jump()
+                        if not game.playing:
+                            game.start()
+
+                if event.key == pygame.K_r:
+                    game.restart()
+                    dino = game.dino
+                    loops = 0
+                    over = False
 
         clock.tick(80)
         pygame.display.update()
 
-main()
 
+main()
